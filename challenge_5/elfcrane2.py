a075a5d57de4074f9ad4d8086b9eb62941eccf0f
EventHor1zon <3v3ntH0r1z0n@protonmail.com>
2022-12-06 23:18:34 +0000

    chal 5 - fun but too much text parsing :/

---
+++ challenge_5/elfcrane2.py
@@ -0,0 +1,89 @@
+
+import sys
+import re
+
+def parse_instructions(instr: str) -> list:
+    instruction_list = []
+    for line in instr.split("\n")[:-1]:
+        digits = [int(s) for s in re.findall(r'\d+', line)]
+        instruction_list.append({
+            'num': digits[0],
+            'source': digits[1]-1,
+            'dest': digits[2]-1
+        })
+    # print(instruction_list)
+    return instruction_list
+
+
+def parse_row(line: str):
+    out = ""
+    ## just keep specific columns - these
+    ## are 1,5,9 etc. x % 4 = 1
+    for x in range(len(line)):
+        if x % 4 == 1:
+            out += line[x]
+    return out
+
+def stacks_to_lists(stacks):
+    ## remove square brackets and spaces
+    stack_list = []
+
+    rows = []
+    for line in stacks.split("\n"):
+        rows.append(parse_row(line))
+
+    ## transpose the rows into stacks
+
+    ## we have the list of rows top to bottom
+    ## they should all be same len
+    for index in range(len(rows[0])):
+        stack = []
+        for row in rows:
+            if row[index] != " " and row[index].isalpha():
+                stack.append(row[index])
+        ## reverse so they're in stack order
+        stack.reverse()
+        stack_list.append(stack)
+    
+    print(stack_list)
+    return stack_list
+
+def perform_instructions(stacks, dirs):
+
+    counter = 0
+    for dir in dirs:
+        tmp = []
+        for i in range(dir['num']):
+            item = stacks[dir['source']].pop()
+            tmp.append(item)
+        tmp.reverse()
+        stacks[dir['dest']].extend(tmp)
+        print(f"counter {counter}")
+        counter += 1
+    return stacks
+
+def split_file(file):
+    with open(file) as f:
+        input = f.read()
+    return input.split("\n\n")
+
+
+def main(infile):
+    
+    stacks, instructions = split_file(infile)
+
+    stack_lists = stacks_to_lists(stacks)
+
+    instruction_list = parse_instructions(instructions)
+
+    final_stacks = perform_instructions(stack_lists, instruction_list)
+
+    print(final_stacks)
+    
+    answer = ""
+    for stack in final_stacks:
+        answer += stack.pop()
+    print(f"Answer: {answer}")
+
+if __name__ == '__main__':
+    main(sys.argv[1])
